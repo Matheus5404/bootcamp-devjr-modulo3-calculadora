2be333c31a879a300927e686e50ecaf1b969ea75
Seu Nome <seuemail@exemplo.com>
2025-08-04 19:17:10 -0300

    closes #4 - Implementar as 4 operacoes

--- javascript.js
+++ javascript.js
@@ -1,8 +1,34 @@
-function calcular() {
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
+function calcular(operacao) {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const output = document.getElementById("output");
+    let resultado;
 
-    var output = num1 + num2;
-    
-    document.getElementById("output").value = output;
+    if (isNaN(num1) || isNaN(num2)) {
+        output.value = "Valores inválidos";
+        return;
+    }
+
+    switch (operacao) {
+        case '+':
+            resultado = num1 + num2;
+            break;
+        case '-':
+            resultado = num1 - num2;
+            break;
+        case '*':
+            resultado = num1 * num2;
+            break;
+        case '/':
+            if (num2 === 0) {
+                output.value = "Divisão por zero!";
+                return;
+            }
+            resultado = num1 / num2;
+            break;
+        default:
+            resultado = "Operação inválida";
+    }
+
+    output.value = resultado;
 }
